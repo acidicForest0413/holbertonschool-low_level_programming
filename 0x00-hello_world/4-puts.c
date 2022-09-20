c131df316a2f286799f8c3e64f93abcfa93b630d
Victoria Cook <5540@holbertonstudents.com>
2022-09-20 08:19:30 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <studio.h>
-
-int main (void)
+/**kinda like an echo command**/
+int main(void)
 {
-	puts ("\" Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\" Programming is like building a multilingual puzzle");
+return (0);
 }
